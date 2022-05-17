401bb867f95124ff3e0214f86065e3819df6dec6
Hayet Kumasseh <naaakleh@gmail.com>
2022-05-17 11:31:08 -0400

    commit tree f3c9a211bfdf9538ed891c992046148033631167

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate') {
+    return `This Saturday, I want to ${event}!`;
+};
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective( special = "*") {
+    return function (adjective = "special") {
+        return `You are ${special}${adjective}${special}!`;
+    };
+}
+wrapAdjective("||")("a dedicated programmer")
